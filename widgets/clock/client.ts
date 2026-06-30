beab74ae5d5329fb436b10f034c4bc7b3755fd9c
khmilevoi <khmilevoi@gmail.com>
2026-07-01 01:33:27 +0200

    refactor(widgets): move defineWidgetClient into widget-sdk

--- widgets/clock/client.ts
+++ widgets/clock/client.ts
@@ -1,4 +1,4 @@
-import { defineWidgetClient } from '@/widget-registry/model/widget-definition'
+import { defineWidgetClient } from '@widget-sdk/define-widget-client'
 
 import type { ClockEvents } from './types'
 
